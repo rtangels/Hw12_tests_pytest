faa9a05bcef1382084c28522e44dba2288b3f937
rtangels <VanshinChaineezMaster@yandex.ru>
2023-03-28 00:33:55 +0300

    cover all arrs

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -6,6 +6,11 @@ def test_get():
     assert arrs.get([], -1, "test") == "test"
 
 
+
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([],1) == []
+    assert arrs.my_slice([1,2,3],-4) == [1,2,3]
+    assert arrs.my_slice([1,2,3],-2) == [2,3]
+
